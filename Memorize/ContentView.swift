4cbf2b2144f2cea1e1c2468272265bc896e5937f
Roland Schmitz <rola@2rs.de>
2020-05-30 18:00:14 +0200

    Empty Project with gitignore

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Roland Schmitz on 30.05.20.
+//  Copyright © 2020 Roland Schmitz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
