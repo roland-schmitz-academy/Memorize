ed58f03a0748aa95504f5ecc4d925c49c08de71f
Roland Schmitz <rola@2rs.de>
2020-05-31 11:42:36 +0200

    Lecture 3: adapt emoji font size to view size, refactor magic numbers

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -15,6 +15,7 @@ struct EmojiMemoryGameView: View {
             HStack {
                 ForEach(viewModel.cards) { card in
                     CardView(card: card)
+                        .aspectRatio(2 / 3, contentMode: .fit)
                         .onTapGesture {
                             self.viewModel.choose(card: card)
                         }
@@ -22,7 +23,6 @@ struct EmojiMemoryGameView: View {
             }
                 .padding()
                 .foregroundColor(Color(.systemOrange))
-                .font(self.viewModel.cards.count < 10 ? .largeTitle : .headline)
 
             Button(action: { self.viewModel.shuffleCards() }) {
                 Text("Shuffle").padding().background(Color(.systemBlue)).foregroundColor(Color(.systemBackground)).cornerRadius(10)
@@ -36,16 +36,32 @@ struct CardView : View {
     var card: EmojiMemoryGame.Card
 
     var body: some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10).fill(Color(.secondarySystemBackground))
-                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
-                Text(card.content)
-            } else {
-                RoundedRectangle(cornerRadius: 10).fill()
-            }
-        }.aspectRatio(2/3, contentMode: .fit)
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
     }
+
+    private func body(for size: CGSize) -> some View {
+        ZStack {
+            if self.card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color(.secondarySystemBackground))
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(self.card.content)
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }.font(.system(size: fontSize(for: size)))
+    }
+
+    // MARK: - Card drawing constants
+
+    let cornerRadius: CGFloat = 10
+    let edgeLineWidth: CGFloat = 3
+
+    private func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.7
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
