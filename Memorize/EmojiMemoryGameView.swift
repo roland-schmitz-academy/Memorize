f9407b42ad8b14b2911869c5646cc9d158d02ac0
Roland Schmitz <rola@2rs.de>
2020-06-03 21:55:04 +0200

    Lecture 4, Assignment 2, Task 10: Cleanup UI

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -11,11 +11,13 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             HStack {
-                Text(viewModel.theme.name).font(.largeTitle).foregroundColor(Color(viewModel.theme.color)).padding()
-                Text("Score: \(viewModel.score)").font(.title).foregroundColor(Color(viewModel.theme.color)).padding()
-            }
+                Text(viewModel.theme.name).font(.title).foregroundColor(Color(viewModel.theme.color))
+                Spacer()
+                Text("Score: \(viewModel.score)").font(.title).foregroundColor(Color(viewModel.theme.color))
+            }.padding([.horizontal])
+            Divider()
             Grid(viewModel.cards) { card in
                 CardView(card: card)
                     .padding(5)
@@ -24,16 +26,16 @@ struct EmojiMemoryGameView: View {
                         self.viewModel.choose(card: card)
                     }
             }
-                .padding()
+                //.padding()
                 .foregroundColor(Color(viewModel.theme.color))
-
+            Divider()
             HStack {
                 Button(action: { self.viewModel.newGame() }) {
-                    Text("New Game").padding().background(Color(.systemBlue)).foregroundColor(Color(.systemBackground)).cornerRadius(10)
-                }.padding()
+                    Text("New Game").padding(.vertical, 5).padding(.horizontal,20).background(Color(.systemBlue)).foregroundColor(Color(.systemBackground)).cornerRadius(10)
+                }.padding(5)
                 Button(action: { self.viewModel.repeatGame() }) {
-                    Text("Shuffle").padding().background(Color(.systemBlue)).foregroundColor(Color(.systemBackground)).cornerRadius(10)
-                }.padding()
+                    Text("Shuffle").padding(.vertical, 5).padding(.horizontal,30).background(Color(.systemBlue)).foregroundColor(Color(.systemBackground)).cornerRadius(10)
+                }.padding(5)
             }
         }
     }
