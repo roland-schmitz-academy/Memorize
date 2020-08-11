d93c40681f9eeaced575056fe47bf92f459027a5
Roland Schmitz <rola@2rs.de>
2020-08-11 11:23:53 +0200

    Assignment 5, Task 1: remove possibility of random pair count in themes

--- Memorize/Theme.swift
+++ Memorize/Theme.swift
@@ -9,13 +9,13 @@ import SwiftUI
 struct Theme {
     let name: String
     let emojis: [String]
-    let numberOfPairs: Int?
+    let numberOfPairs: Int/*?*/
     let gradient: Gradient
     var color: Color {
         gradient.stops.first!.color
     }
 
-    init(_ name: String, emojis: String, numberOfPairs: Int? = nil, color: Color) {
+    init(_ name: String, emojis: String, numberOfPairs: Int/*? = nil*/, color: Color) {
         self.name = name
         self.emojis =  emojis.map { String($0) }
         print("emojis = \(emojis)")
@@ -23,7 +23,7 @@ struct Theme {
         self.numberOfPairs = numberOfPairs
         self.gradient = Gradient(colors: [color, color])
     }
-    init(_ name: String, emojis: String, numberOfPairs: Int? = nil, gradient: Gradient) {
+    init(_ name: String, emojis: String, numberOfPairs: Int/*? = nil*/, gradient: Gradient) {
         self.name = name
         self.emojis =  emojis.map { String($0) }
         print("emojis = \(emojis)")
@@ -37,7 +37,7 @@ let themes: [Theme] = [
     Theme("Halloween", emojis: "💀🎃👻☠️", numberOfPairs: 4, gradient: Gradient(colors: [Color(.systemOrange), Color(.systemRed),Color(.systemOrange)])),
     Theme("Flags", emojis: "🇮🇹🇪🇺🇳🇱🇦🇺🇧🇷🇩🇪🇦🇹", numberOfPairs: 6, gradient: Gradient(colors: [Color(.systemRed), Color(.systemGreen),Color(.systemBlue),Color(.systemYellow),Color(.systemOrange)])),
     Theme("Animals", emojis: "🐨🐶🐷🐸🐹🐻🐼🐰🦊🐭😺🦋🙉🦎", numberOfPairs: 12, color: Color(.systemGreen)),
-    Theme("Smileys", emojis: "😁😂😃😄😆😇😉😊😌😍😎😐😑😒😕🤔🤗😗🤥😳🙂🙁🙃🙄☺️☹️", color: Color(.systemYellow)),
+    Theme("Smileys", emojis: "😁😂😃😄😆😇😉😊😌😍😎😐😑😒😕🤔🤗😗🤥😳🙂🙁🙃🙄☺️☹️", numberOfPairs: 7, color: Color(.systemYellow)),
     Theme("People", emojis: "👨‍🍳👨‍🎓👨‍🎤👨‍🎨👨‍🚀🤵👩‍🍳👩‍🏫👩‍💻👨🏻‍💻👩‍🎨👨‍🎨", numberOfPairs: 10, color: Color(.systemPurple)),
     Theme("Activity", emojis: "⚽️🎱🏀🏈⚾️🎾🏐🏉🥏", numberOfPairs: 8, color: Color(.systemRed)),
 ]
